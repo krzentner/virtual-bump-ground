11b3b568a66ad933dbd8c31c009cdf52a39914f2
K.R. Zentner <krzentner@gmail.com>
2019-12-11 18:37:33 -0800

    Add "total" csv files, and scripts to generate

---
+++ linux-bin/process_user_logs_totals.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+import csv
+import pdb
+
+vibration = []
+no_vibration = []
+
+keys = ['dt_total', 'x_offset', 'z_offset', 'angle_offset', 'dx_total',
+        'dz_total']
+
+
+def get_table(lines):
+    table = {k: [] for k in keys}
+    for line in lines:
+        for key in keys:
+            if key in line:
+                try:
+                    key, value = line.split('\t')
+                    value = float(value)
+                    table[key] = value
+                except ValueError:
+                    pass
+    return table
+
+
+for user_n in range(1, 9):
+    print('user_n', user_n)
+    with open('user{}_vibration.log'.format(user_n)) as f:
+        vibration.append(get_table(f))
+    with open('user{}_no_vibration.log'.format(user_n)) as f:
+        no_vibration.append(get_table(f))
+
+
+def write_csv(table, filename):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(keys + ['user_id'])
+        for user_id, user in enumerate(table):
+            writer.writerow([user[key] for key in keys] + [user_id])
+
+
+write_csv(vibration, 'vibration_total.csv')
+write_csv(no_vibration, 'no_vibration_total.csv')
